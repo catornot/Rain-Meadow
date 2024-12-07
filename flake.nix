67a0788cb8f7e76f9bdd21ac7d3104ebc2ddba19
catornot <41955154+catornot@users.noreply.github.com>
2024-12-06 22:29:06 -0500

    move lsp into system config

--- flake.nix
+++ flake.nix
@@ -4,14 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils = { url = "github:numtide/flake-utils"; };
-
-    roslyn-language-server = {
-      url = "github:sofusa/roslyn-language-server";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, roslyn-language-server}: 
+  outputs = { self, nixpkgs, flake-utils}: 
      flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -22,7 +17,6 @@
         devShell = pkgs.mkShell {
           packages = [
             dotnet
-            roslyn-language-server.packages.${system}.roslyn-language-server
           ];
         };
       });
